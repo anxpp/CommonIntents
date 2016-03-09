f6aa3be366743fabd48692cb008fb51c8e0f2a6d
anxpp <anxppp@163.com>
2016-03-09 18:05:29 +0800

    添加部分intent

--- app/src/main/java/com/anxpp/commonintents/MainActivity.java
+++ app/src/main/java/com/anxpp/commonintents/MainActivity.java
@@ -1,10 +1,16 @@
 package com.anxpp.commonintents;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
@@ -13,14 +19,24 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.SimpleAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anxpp.commonintents.utils.IntentUtils;
 import com.anxpp.commonintents.utils.SdkException;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final int CAMERA_THUMBNAIL_RESULT = 0;
+    public static String items[] = {
+            "Alarm","Timer","showAlarms",
+            "addCalenderEvent","CapturePicture","Storage",
+            "Maps","Music","Video",
+            "Phone","Setting","Message",
+            "Web","Other"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +51,11 @@ public class MainActivity extends AppCompatActivity {
     private  void toast(Object msg){
         Toast.makeText(MainActivity.this,""+msg, Toast.LENGTH_SHORT).show();
     }
-
     private void init(){
         //设置标题栏
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.mipmap.ic_launcher);
+//        toolbar.setLogo(R.mipmap.ic_launcher);
+//        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.colorAccent));
         setSupportActionBar(toolbar);
         //设置fab按钮
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
@@ -56,21 +72,74 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
                     case 0://Alarm
-                        toast("设置闹钟");
+                        toast(items[position]);
                         IntentUtils.createAlarm(MainActivity.this, "测试", 7, 27);
                         break;
                     case 1://Timer
                         try {
-                            IntentUtils.startTimer(MainActivity.this,"测试",20);
-                            toast("开始计时器");
+                            IntentUtils.startTimer(MainActivity.this,"测试",20,true);
+                            toast(items[position]);
                         } catch (SdkException e) {
-                            toast("SDK版本太低");
+                            toast(e.getMessage());
                         }
+                        break;
+                    case 2://ShowAllAlarm
+                        try {
+                            IntentUtils.showAllAlarm(MainActivity.this);
+                            toast(items[position]);
+                        } catch (SdkException e) {
+                            toast(e.getMessage());
+                        }
+                        break;
+                    case 3://addCalenderEvent
+                        Calendar startCalendar = Calendar.getInstance();
+                        startCalendar.set(2016,3,8,8,30);
+                        Calendar endCalendar = Calendar.getInstance();
+                        endCalendar.set(2016,3,8,8,30);
+                        IntentUtils.addCalenderEvent(MainActivity.this,"title","location",startCalendar,endCalendar);
+                        toast(items[position]);
+                        break;
+                    case 4://CapturePicture
+                        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_THUMBNAIL_RESULT);
+                        toast(items[position]);
                         break;
                     default:break;
                 }
             }
         });
+    }
+    //取回数据
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("onActivityResult", "");
+        Log.e("requestCode", requestCode + "");
+        Log.e("resultCode", resultCode + "");
+        if(resultCode == RESULT_OK){
+            switch (requestCode) {
+                case CAMERA_THUMBNAIL_RESULT:
+                    Bundle bundle = data.getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    onCreateDialog(bitmap).show();
+                    break;
+                default:break;
+            }
+        }
+    }
+    private Dialog onCreateDialog(Bitmap bitmap) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_img, null);
+        ((ImageView)view.findViewById(R.id.img_dialog)).setImageBitmap(bitmap);
+        builder.setView(view);
+        final Dialog dialog = builder.create();
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        return dialog;
     }
 
     @Override
@@ -92,23 +161,17 @@ class MyAdapter extends BaseAdapter{
     private LayoutInflater inflater;
 
     Context context;
-    public static String items[] = {
-            "Alarm","Timer","Camera",
-            "Contacts","Email","Storage",
-            "Maps","Music","Video",
-            "Phone","Setting","Message",
-            "Web","Other"};
     public MyAdapter(Context context){
         this.context = context;
         inflater = LayoutInflater.from(context);
     }
     @Override
     public int getCount() {
-        return items.length;
+        return MainActivity.items.length;
     }
     @Override
     public Object getItem(int position) {
-        return items[position];
+        return MainActivity.items[position];
     }
     @Override
     public long getItemId(int position) {
@@ -125,7 +188,7 @@ class MyAdapter extends BaseAdapter{
         }else{
             viewHolder = (ViewHolder)convertView.getTag();
         }
-        viewHolder.textView.setText(items[position]);
+        viewHolder.textView.setText(MainActivity.items[position]);
         return convertView;
     }
 
--- app/src/main/java/com/anxpp/commonintents/utils/IntentUtils.java
+++ app/src/main/java/com/anxpp/commonintents/utils/IntentUtils.java
@@ -2,8 +2,13 @@ package com.anxpp.commonintents.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.AlarmClock;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
+
+import java.util.Calendar;
 
 /**
  * 常用Intent
@@ -32,17 +37,53 @@ public class IntentUtils {
      * @param context 上下文
      * @param msg 消息
      * @param sec 时间(秒)
+     * @param ui true为后台计时(通常通知栏会有提示)，false会弹出计时界面
      */
-    public static void startTimer(Context context,String msg,int sec) throws SdkException {
+    public static void startTimer(Context context,String msg,int sec,boolean ui) throws SdkException {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER);
-            intent.putExtra(AlarmClock.EXTRA_LENGTH,sec);
-            intent.putExtra(AlarmClock.EXTRA_MESSAGE,msg);
-            intent.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
+            Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+                    .putExtra(AlarmClock.EXTRA_LENGTH,sec)
+                    .putExtra(AlarmClock.EXTRA_MESSAGE,msg)
+                    .putExtra(AlarmClock.EXTRA_SKIP_UI,ui);
             if(intent.resolveActivity(context.getPackageManager())!=null)
                 context.startActivity(intent);
         }
-        else throw new SdkException("dfg");
+        else throw new SdkException("SDK版本不能低于" + android.os.Build.VERSION_CODES.KITKAT);
+    }
+
+    /**
+     * 显示所有闹钟
+     * @param context 上下文
+     * @throws SdkException
+     */
+    public static void showAllAlarm(Context context) throws SdkException {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+            if (intent.resolveActivity(context.getPackageManager()) != null)
+                context.startActivity(intent);
+            else throw new SdkException("当前版本不支持");
+        }
+        else throw new SdkException("SDK版本不能低于" + android.os.Build.VERSION_CODES.KITKAT);
+    }
+
+    /**
+     * 添加日历事件
+     * @param context 上下文
+     * @param title 标题
+     * @param location 位置
+     * @param begin 开始
+     * @param end 结束
+     */
+    public static void addCalenderEvent(Context context, String title, String location, Calendar begin, Calendar end) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(intent);
+        }
     }
 
 }
